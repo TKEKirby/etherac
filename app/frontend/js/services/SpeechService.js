a523c3056f5710702bf61c7dfcd3d9c30f155e84
Eric <TKEKirby552@gmail.com>
2016-04-16 02:09:48 -0600

    changed some commands

--- app/frontend/js/services/SpeechService.js
+++ app/frontend/js/services/SpeechService.js
@@ -1,32 +1,113 @@
 'use strict';
 
 angular.module('etherac').service('SpeechService', ['$rootScope','MusicService', function ($rootScope, MusicService) {
-	var compareSongTitles = function (title, songList) {
-		title = title.toLowerCase();
-		title = title.replace('-', ' ');
-		title = title.replace(',', '');
-		for (var i = 0; i < songList.length; i++) {
-			var songListTitle = songList[i].title.toString().toLowerCase();
-			songListTitle = songListTitle.toString().split(' (', 1);
-			songListTitle = songListTitle.toString().split(' ft', 1);
-			songListTitle = songListTitle.toString().split(' ft.', 1);
-			songListTitle = songListTitle.toString().split(' feat', 1);
-			songListTitle = songListTitle.toString().split(' with', 1);
-			songListTitle = songListTitle.toString().split(' w-', 1);
-			songListTitle = songListTitle.toString().replace(',', '');
-			songListTitle = songListTitle.toString();
-			if (title.includes(songListTitle)) {
-				return i;
-			} else if (songListTitle.includes(title)) {
-				return i;
+
+	var titlePlayCommandList = [];
+	var titleAddCommandList = [];
+	var artistCommandList = [];
+
+	/*
+	* Description:
+	*
+	* Params:
+	* Return:
+	*/
+	function getTitleAddCommands() {
+		var titles = [];
+		MusicService.readLibrary().then(function (res) {
+			for (var i = 0; i < res.length; i++) {
+				if (titles.indexOf(res[i].title.toString()) === -1)  {
+					var songTitle = res[i].title.toString().toLowerCase();
+					songTitle = songTitle.toString().split(' (', 1);
+					songTitle = songTitle.toString().split(' ft', 1);
+					songTitle = songTitle.toString().split(' ft.', 1);
+					songTitle = songTitle.toString().split(' feat', 1);
+					songTitle = songTitle.toString().split(' with', 1);
+					songTitle = songTitle.toString().split(' w-', 1);
+					songTitle = songTitle.toString().replace(',', '');
+					titles.push(songTitle);
+				}
 			}
-		}
-		return -1;
-	};
+			for (var j = 0; j < titles.length; j++) {
+				titleAddCommandList[j] = 'Add ' + titles[j].toString();
+			}
+		});
+	}
+
+	/*
+	* Description:
+	*
+	* Params:
+	* Return:
+	*/
+	function getTitlePlayCommands() {
+		var titles = [];
+		MusicService.readLibrary().then(function (res) {
+			for (var i = 0; i < res.length; i++) {
+				if (titles.indexOf(res[i].title.toString()) === -1)  {
+					var songTitle = res[i].title.toString().toLowerCase();
+					songTitle = songTitle.toString().split(' (', 1);
+					songTitle = songTitle.toString().split(' ft', 1);
+					songTitle = songTitle.toString().split(' ft.', 1);
+					songTitle = songTitle.toString().split(' feat', 1);
+					songTitle = songTitle.toString().split(' with', 1);
+					songTitle = songTitle.toString().split(' w-', 1);
+					songTitle = songTitle.toString().replace(',', '');
+					titles.push(songTitle);
+				}
+			}
+			for (var j = 0; j < titles.length; j++) {
+				titlePlayCommandList[j] = 'Play ' + titles[j].toString();
+			}
+		});
+	}
+
+
+	/*
+	* Description:
+	*
+	* Params:
+	* Return:
+	*/
+	function getArtistCommands() {
+		var artists = [];
+		MusicService.readLibrary().then(function (res) {
+			for (var i = 0; i < res.length; i++) {
+				if (artists.indexOf(res[i].artist.toString()) === -1)  {
+					artists.push(res[i].artist.toString());
+				}
+			}
+			for (var j = 0; j < artists.length; j++) {
+				artistCommandList[j] = 'Play all the songs by ' + artists[j].toString();
+			}
+		});
+	}
+
+	/*
+	* Description:
+	*
+	* Params:
+	* Return:
+	*/
+	function formatString(str) {
+		str = str.toString().toLowerCase();
+		str = str.toString().split(' (', 1);
+		str = str.toString().split(' ft', 1);
+		str = str.toString().split(' ft.', 1);
+		str = str.toString().split(' feat', 1);
+		str = str.toString().split(' with', 1);
+		str = str.toString().split(' w-', 1);
+		str = str.toString().replace(',', '');
+		return str;
+	}
 
 	return {
 
 		setupSpeech:function(){
+
+			getTitlePlayCommands();
+			getTitleAddCommands();
+			getArtistCommands();
 
 			/*
 			* Description:
@@ -127,19 +208,20 @@ angular.module('etherac').service('SpeechService', ['$rootScope','MusicService',
 				* Response:
 				*/
 				{
-					smart:true,
-					indexes:['play *'],
-					action:function(i,wildcard){
-						var str = wildcard.toString();
+					indexes:titlePlayCommandList,
+					action:function(i){
+						var str = titlePlayCommandList[i].toString().replace('Play ', '');
 						MusicService.readLibrary().then(function (res) {
 							if (res !== 'error') {
-								var indexOfSong = compareSongTitles(str, res);
-								if(indexOfSong !== -1){
-									MusicService.addPlayToNowPlaying(res[indexOfSong]);
-								}else{
-									artyom.say('That is not a Song I have in the library.');
-									//artyom.say('No');
+								for (var i = 0; i < res.length; i++) {
+									var title = formatString(res[i].title);
+									if (title === str) {
+										MusicService.addPlayToNowPlaying(res[i]);
+									}
 								}
+							}else{
+								artyom.say('That is not an song I have in the library.');
+								//artyom.say('No');
 							}
 						});
 					}
@@ -151,19 +233,45 @@ angular.module('etherac').service('SpeechService', ['$rootScope','MusicService',
 				* Response:
 				*/
 				{
-					smart:true,
-					indexes:['play *'],
-					action:function(i,wildcard){
-						var str = wildcard.toString();
+					indexes:titleAddCommandList,
+					action:function(i){
+						var str = titleAddCommandList[i].toString().replace('Add ', '');
 						MusicService.readLibrary().then(function (res) {
 							if (res !== 'error') {
-								var indexOfSong = compareSongTitles(str, res);
-								if(indexOfSong !== -1){
-									MusicService.addToNowPlaying(res[indexOfSong]);
-								}else{
-									artyom.say('That is not a Song I have in the library.');
-									//artyom.say('No');
+								for (var i = 0; i < res.length; i++) {
+									var title = formatString(res[i].title);
+									if (title === str) {
+										MusicService.addToNowPlaying(res[i]);
+									}
 								}
+							}else{
+								artyom.say('That is not an song I have in the library.');
+								//artyom.say('No');
+							}
+						});
+					}
+				},
+
+				/*
+				* Command Description:
+				* Command:
+				* Response:
+				*/
+				{
+					indexes:artistCommandList,
+					action:function(i){
+						var str = artistCommandList[i].toString().replace('Play all the songs by ', '');
+						MusicService.readLibrary().then(function (res) {
+							if (res !== 'error') {
+								for (var i = 0; i < res.length; i++) {
+									if (res[i].artist === str) {
+										MusicService.addToNowPlaying(res[i]);
+									}
+								}
+								MusicService.playNowPlaying();
+							}else{
+								artyom.say('That is not an Artist I have in the library.');
+								//artyom.say('No');
 							}
 						});
 					}
@@ -211,18 +319,18 @@ angular.module('etherac').service('SpeechService', ['$rootScope','MusicService',
 				* Command:
 				* Response:
 				{
-					smart:true,
-					indexes:['play'],
-					action:function(){
+				smart:true,
+				indexes:['play'],
+				action:function(){
 
-					}
-				}
-				*/
-
-
-			]);
-
+			}
 		}
-	};
+		*/
+
+
+	]);
+
+}
+};
 }
 ]);
